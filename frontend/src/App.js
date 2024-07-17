7d1552d0adc72bc234e8d77babec30e448ddeb80
Junaid Syed <sjunaid626@gmail.com>
2024-07-17 21:24:20 +0530

    Move the api logic from App

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,36 +1,8 @@
-import React, { useEffect, useState } from "react";
-import { getAllTasks, updateTask } from "./api/tasks";
+import React from "react";
 import Tasks from "./components/tasks/tasks";
 import NewTask from "./components/tasks/new-task";
 
 export default function App() {
-  // const [tasks, setTasks] = useState([]);
-  // const [error, setError] = useState(null);
-
-  // useEffect(() => {
-  //   const fetchTasks = async () => {
-  //     try {
-  //       const tasksData = await getAllTasks();
-  //       setTasks(tasksData);
-  //     } catch (err) {
-  //       setError("Failed to fetch tasks");
-  //     }
-  //   };
-
-  //   fetchTasks();
-  // }, []);
-
-  // const handleUpdateClick = async (e, task) => {
-  //   e.preventDefault();
-  //   const taskId = task.id;
-  //   const status = task.status;
-  //   await updateTask(taskId, status);
-  //   onClose();
-  // };
-
-  // if (error) {
-  //   return <div>{error}</div>;
-  // }
 
   return (
     <div className="h-screen flex flex-col items-center justify-around bg-gray-300">
